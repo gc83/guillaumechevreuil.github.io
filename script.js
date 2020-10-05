28d5b2552c2296ecd16c8bdf35b7ed730cd293d6
gc83 <guillaume.ch83@gmail.com>
2020-10-05 20:52:24 +0200

    white-dom

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-//paralax
+//paralax hero
 var scene = document.getElementById('posterParallax');
 var parallax = new Parallax(scene, {
     selector: '.layer'
+});
+
+//paralax webdesign
+var scene = document.getElementById('designParallax');
+var parallax = new Parallax(scene, {
+    selector: '.layer'
+});
+
+//paralax model 3d
+var scene = document.getElementById('modelParallax');
+var parallax = new Parallax(scene, {
+    selector: '.layer'
+});
+
+//paralax intégration
+var scene = document.getElementById('devParallax');
+var parallax = new Parallax(scene, {
+    selector: '.layer'
+});
+
+
+//lottie
+LottieInteractivity.create({
+    player: '#mouseMove',
+    mode: 'cursor',
+    actions: [{
+        position: {
+            x: [0, 1],
+            y: [0, 1]
+        },
+        type: 'seek',
+        frames: [-100, 250],
+    }, ],
 });
